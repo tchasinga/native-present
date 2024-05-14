1f47995f60f61439cf26efd765d54575fe116b54
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-14 16:13:36 +0200

    new change's is added now

--- src/Pages/Navbars.jsx
+++ src/Pages/Navbars.jsx
@@ -5,7 +5,7 @@ export default function Navbars() {
   return (
     <div className="flex justify-between py-3 p-3 items-center w-full">
       <div className="text-3xl font-extrabold">
-        <FaReact />
+        <FaReact className="spinerCode"/>
       </div>
 
       <div className="flex items-center gap-5 text-xl">
