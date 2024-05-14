ccde58683cf8af392c3a68e67e4da132eeb50fca
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-14 15:27:34 +0200

    new code is just added now

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './index.css'
+import Navbars from './Pages/Navbars'
 
 
 function App() {
 
   return (
     <>
+      <Navbars />
     </>
   )
 }
---
+++ src/Pages/Navbars.jsx
@@ -0,0 +1,9 @@
+// import { FaReact } from "react-icons/fa";
+
+export default function Navbars() {
+  return (
+    <div className="">
+        <h1 className="text-3xl">Balolebwami</h1>
+    </div>
+  )
+}
