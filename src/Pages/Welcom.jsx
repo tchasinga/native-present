f29b736ce0b4d83c2785f33a37a0fc74ff0f711e
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-14 16:46:49 +0200

    new code is added now

--- src/Pages/Welcom.jsx
+++ src/Pages/Welcom.jsx
@@ -3,7 +3,7 @@ export default function Welcom() {
   return (
     <div className="text-[90px] leading-none min-h-screen border flex flex-col pt-28 font-bold w-full p-3">
        <h1 className="">Welcome to the roadmap</h1>
-         <h1 className="">This is a roadmap to help you learn and master react-native</h1>
+         <h1 className="">This is a roadmap to help you learn and master React-native</h1>
     </div>
   )
 }
