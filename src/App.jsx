5e61821c86706dd2f6c74e98f7e885ac3f4e177c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-14 15:45:29 +0200

    New code is added now

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbars from './Pages/Navbars'
 function App() {
 
   return (
-    <div className='mx-auto max-w-5xl'>
+    <div className='mx-auto max-w-7xl border'>
       <Navbars />
     </div>
   )
--- src/Pages/Navbars.jsx
+++ src/Pages/Navbars.jsx
@@ -1,9 +1,12 @@
-// import { FaReact } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
 
 export default function Navbars() {
   return (
     <div className="">
-        <h1 className="text-3xl">Balolebwami</h1>
+      <div className="">
+        <FaReact />
+      </div>
+      <div className=""></div>
     </div>
   )
 }
