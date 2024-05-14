9dd9b75bb3589cbe2387a2bf55c7756effd6f607
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-14 16:05:30 +0200

    new one commit

--- src/Pages/Navbars.jsx
+++ src/Pages/Navbars.jsx
@@ -1,10 +1,11 @@
+import { FaReact } from "react-icons/fa";
 
 export default function Navbars() {
   return (
     <div className="">
 
       <div className="">
-      
+        <FaReact />
       </div>
 
       <div className=""></div>
