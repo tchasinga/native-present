9cfb5fd65f37ac2cc2a52970534b8238d853ee13
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-14 15:45:53 +0200

    new valid code

--- src/Pages/Navbars.jsx
+++ src/Pages/Navbars.jsx
@@ -3,9 +3,11 @@ import { FaReact } from "react-icons/fa";
 export default function Navbars() {
   return (
     <div className="">
+      
       <div className="">
         <FaReact />
       </div>
+
       <div className=""></div>
     </div>
   )
