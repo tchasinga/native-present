2aaa6b5b18cf0a1ef24db5f697da03135dc16d34
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-14 16:39:43 +0200

    new code is added now

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './index.css'
 import Navbars from './Pages/Navbars'
+import Welcom from './Pages/Welcom'
 
 
 function App() {
 
   return (
-    <div className='mx-auto max-w-7xl'>
+    <div className='mx-auto max-w-7xl w-full'>
       <Navbars />
+      <Welcom />
     </div>
   )
 }
