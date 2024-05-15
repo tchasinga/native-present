40ebf5dccab0e9865a667f6196095d406667b718
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-15 11:26:34 +0200

    New code is added now

--- src/Pages/Welcom.jsx
+++ src/Pages/Welcom.jsx
@@ -6,7 +6,7 @@ export default function Welcom() {
     <motion.div 
     initial="offscreen"
     whileInView={"onscreen"}
-    variants={containerVariant}
+    variants={containerVariant()}
     className="text-[90px] leading-none min-h-screen flex flex-col pt-28 font-bold w-full p-3 text-slate-900">
        <h1 className="">Welcome to the roadmap</h1>
          <h1 className="">This is a roadmap to help you learn and master React-native</h1>
