bb68597d9227783036de9294060ca658ce5dec04
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-15 10:30:05 +0200

    new commit

--- src/Pages/Program.jsx
+++ src/Pages/Program.jsx
@@ -1,12 +1,16 @@
 export default function Program() {
   return (
-     <div className="flex flex-col pb-16">
+     <div className="flex flex-col pb-16 w-full">
        <h1 className="text-5xl text-slate-900 font-semibold">Roadmap  of the program</h1>
        <hr className="w-28 " />
 
        {/* First week of the program */}
 
-       <div className="flex flex-col"></div>
+       <div className="flex flex-col py-3">
+        <h3 className="font-bold text-slate-900">Introduction and welcome call</h3>
+        <h4 className="text-sm font-medium">First week</h4>
+         
+       </div>
     </div>
   )
 }
