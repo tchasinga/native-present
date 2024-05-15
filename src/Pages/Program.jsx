0bfe50e19ab4ba60d8895e3db5ee6eb09b19e221
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-15 10:19:48 +0200

    New code is added now

--- src/Pages/Program.jsx
+++ src/Pages/Program.jsx
@@ -1,8 +1,12 @@
-
-
 export default function Program() {
   return (
-     <div>      
+     <div className="flex flex-col pb-16">
+       <h1 className="text-5xl text-slate-900 font-semibold">Roadmap  of the program</h1>
+       <hr className="w-28 " />
+
+       {/* First week of the program */}
+
+       <div className="flex flex-col"></div>
     </div>
   )
 }
