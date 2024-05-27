5523c724dc4702be6f06623ebfdb5002b20b5a93
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-27 12:05:46 +0200

    new code is added now

--- src/Pages/Apispages.jsx
+++ src/Pages/Apispages.jsx
@@ -2,10 +2,11 @@
 
 export default function Apispages() {
   return (
-    <div className="flex items-center justify-center pt-52">
-       <div>
-       <h1 className="text-5xl text-slate-900 font-semibold">Please come back right after the first week !!</h1>
-       </div>
+    <div className="flex flex-col items-center justify-center pt-52">
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
+      <div className=""></div>
     </div>
   )
 }
